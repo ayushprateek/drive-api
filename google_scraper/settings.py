c09cf2892887afd249dfe58dbae26afa594e89d6
Ayush Pratik <ayush.kishul@gmail.com>
2024-06-04 21:21:28 +0530

    Added CORS Policy so that app can be run locally

--- google_scraper/settings.py
+++ google_scraper/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hotels',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,9 +49,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'google_scraper.urls'
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
